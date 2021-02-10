d5c424f3409f6e79bf5a231dd6f7f3fff4cbcb6a
Kyle McDowell <kymcd21@icstudents.org>
2021-02-10 02:58:19 +0000

    00000

--- Quiz.java
+++ Quiz.java
@@ -1,42 +1,40 @@
 /**
-   A coin with a monetary value.
-*/
-public class Coin implements Measurable
-{
-   private double value;
-   private String name;
+ * A coin with a monetary score.
+ */
+public class Quiz implements Measurable {
+  private double score;
+  private String grade;
 
-   /**
-      Constructs a coin.
-      @param aValue the monetary value of the coin
-      @param aName the name of the coin
+  /**
+   * Constructs a coin.
+   * 
+   * @param aScore the monetary score of the coin
+   * @param agrade the grade of the coin
    */
-   public Coin(double aValue, String aName) 
-   { 
-      value = aValue; 
-      name = aName;
-   }
+  public Quiz(double aScore, String aGrade) {
+    score = aScore;
+    grade = aGrade;
+  }
 
-   /**
-      Gets the coin value.
-      @return the value
+  /**
+   * Gets the coin score.
+   * 
+   * @return the score
    */
-   public double getScore() 
-   {
-      return value;
-   }
+  public double getScore() {
+    return score;
+  }
 
-   /**
-      Gets the coin name.
-      @return the name
+  /**
+   * Gets the coin grade.
+   * 
+   * @return the grade
    */
-   public String getGrade() 
-   {
-      return name;
-   }
+  public String getGrade() {
+    return grade;
+  }
 
-   public double getAverage() 
-   {
-      return value;
-   }
+  public double getMeasure() {
+    return score;
+  }
 }
